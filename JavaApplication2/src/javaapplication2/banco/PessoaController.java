46fa5124cc3c08b7801ae346b65712671571e600
Administrador <Administrador@10.10.4.18>
2017-07-20 16:38:52 -0300

    Segunda Versão

---
+++ JavaApplication2/src/javaapplication2/banco/PessoaController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2.banco;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Administrador
+ */
+public class PessoaController {
+
+    public boolean inserir() {
+        String sql = "INSERT INTO PESSOA (codigo,nome) VALUES (2, 'Maria')";
+        PreparedStatement ps;
+        try {
+            ps = Conexao.getConexao().prepareStatement(sql);
+            ps.execute();
+            return true;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(PessoaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+//psvm
+
+    public static void main(String[] args) {
+        PessoaController pc = new PessoaController();
+        boolean resultado = pc.inserir();
+        System.out.println("resultado " + resultado);
+    }
+}
